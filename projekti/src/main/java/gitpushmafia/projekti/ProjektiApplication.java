2d43a2b6ffbeb41fdc093eee4eb020fc2d59c6fc
woweezywoo <bhi054@myy.haaga-helia.fi>
2025-10-21 16:30:58 +0300

    Spring Project created

---
+++ projekti/src/main/java/gitpushmafia/projekti/ProjektiApplication.java
@@ -0,0 +1,13 @@
+package gitpushmafia.projekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektiApplication.class, args);
+	}
+
+}
