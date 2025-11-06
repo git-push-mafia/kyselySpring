4e1ce0ef1ccae37257dbf2b10a71b10e66100360
fabiantohamy <fabian.tohamy@hotmail.com>
2025-11-06 12:20:33 +0200

    lisää CrossOrigin react-yhteyttä varten

--- projekti/src/main/java/gitpushmafia/projekti/controllers/KyselyRestController.java
+++ projekti/src/main/java/gitpushmafia/projekti/controllers/KyselyRestController.java
@@ -5,14 +5,18 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import gitpushmafia.projekti.domain.Kysely;
 import gitpushmafia.projekti.domain.KyselyRepository;
 
 @Controller
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class KyselyRestController {
 
     @Autowired
--- projekti/src/main/java/gitpushmafia/projekti/controllers/KysymysRestController.java
+++ projekti/src/main/java/gitpushmafia/projekti/controllers/KysymysRestController.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +14,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import gitpushmafia.projekti.domain.Kysymys;
 import gitpushmafia.projekti.domain.KysymysRepository;
 
+@Controller
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class KysymysRestController {
 
     @Autowired
